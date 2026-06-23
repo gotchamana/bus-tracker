a1c828782381f2599025a615a66d80cd468b9b40
clark.chang <clark.chang@ctitv.com.tw>
2026-06-23 17:56:07 +0800

    fix: Add some ctypto libs

--- lib/Bus/Main.hs
+++ lib/Bus/Main.hs
@@ -7,10 +7,25 @@ import Bus.Logging (withAsyncLogging)
 import Bus.Servant (waiApp)
 import Control.Concurrent.STM (atomically)
 import Control.Concurrent.STM.TChan (dupTChan, newBroadcastTChanIO)
+import Crypto.Store.PKCS12 (SafeBag, SafeContents (SafeContents), readP12File, recover, recoverAuthenticated, unPKCS12)
+import Data.ASN1.Types (ASN1)
+import Data.Coerce (coerce)
 import Network.Wai.Handler.Warp (run)
+
+getSecretKey :: String -> [SafeBag] -> Maybe ASN1
+getSecretKey alias bags = Nothing
 
 defaultMain :: IO ()
 defaultMain = do
+    e <- readP12File "server.pfx"
+
+    let ff = do
+            optAuthP12 <- e
+            (password, pkcs12) <- recoverAuthenticated "secret" optAuthP12
+            bags <- fmap concat . coerce @_ @(_ _ [[SafeBag]]) . recover password . unPKCS12 $ pkcs12
+
+            pure ()
+
     chan <- newBroadcastTChanIO
     duplicatedChan <- atomically (dupTChan chan)
 
