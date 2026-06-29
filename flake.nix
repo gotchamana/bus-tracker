645c73e8caafa3d8522cf5a07b512161b802af30
clark.chang <clark.chang@ctitv.com.tw>
2026-06-29 12:59:06 +0800

    fix: Upgrade hls

--- flake.nix
+++ flake.nix
@@ -11,12 +11,26 @@
         "x86_64-linux"
       ];
       eachSystem = f: nixpkgs.lib.genAttrs supportedSystems f;
+      overlay = final: prev: {
+        all-cabal-hashes = prev.fetchurl (finalAttrs: {
+          url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/4356dd54ddac0d8dc77ab578bfeac5f9148f7b16.tar.gz";
+          sha256 = "YVRxNZkKIOODRoAo39NIqEs/1vY4CPjW7cYAQIgAeVk=";
+          name = "${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
+          pname = "all-cabal-hashes";
+          version = "4356dd5";
+          passthru.updateScript = prev.all-cabal-hashes.updateScript;
+          meta = prev.all-cabal-hashes.meta;
+        });
+      };
     in
     {
       devShells = eachSystem (
         system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          };
           hpkgs = pkgs.haskell.packages.ghc9103;
         in
         {
@@ -32,6 +46,24 @@
             ];
             buildInputs = with pkgs; [ zlib ];
           };
+        }
+      );
+      packages = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          };
+          hpkgs = pkgs.haskell.packages.ghc9124;
+          hpkgs' = hpkgs.extend (
+            self: super: {
+              haskell-language-server = self.callHackage "haskell-language-server" "2.14.0.0" { };
+            }
+          );
+        in
+        {
+          default = hpkgs'.haskell-language-server;
         }
       );
     };
