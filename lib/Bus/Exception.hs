9d6f10faed15db0f04a1c59501675bde93ff840c
shootingstar <jinhung.c@gmail.com>
2026-07-07 16:52:52 +0800

    fix: Temp

--- lib/Bus/Exception.hs
+++ lib/Bus/Exception.hs
@@ -2,6 +2,8 @@ module Bus.Exception (CryptoStoreException (..), isAsyncException, rethrowIO) wh
 
 import Control.Exception (Exception (..), ExceptionWithContext, SomeAsyncException, throwIO)
 import Crypto.Store.Error (StoreError)
+import Data.Aeson (ToJSON)
+import Data.Text (Text)
 import Data.Typeable (typeOf)
 
 newtype NoBacktrace e = NoBacktrace e
@@ -16,6 +18,31 @@ newtype CryptoStoreException = CryptoStoreException StoreError deriving (Show)
 
 instance Exception CryptoStoreException where
     displayException e@(CryptoStoreException err) = show (typeOf e) <> ": " <> show err
+
+data ApiException = ApiException
+    { apiHttpStatus :: Int
+    , apiErrorType :: ErrorType
+    , apiErrorDescription :: Maybe Text
+    , apiErrorDetails :: forall a. (ToJSON a) => a
+    }
+
+instance Show ApiException where
+    show _ = ""
+
+instance Exception ApiException
+
+data ErrorType = ErrorType Text Text Text
+
+etyType :: ErrorType -> Text
+etyType (ErrorType typ _ _) = typ
+
+etyMessageCode :: ErrorType -> Text
+etyMessageCode (ErrorType _ code _) = code
+
+etyMessage :: ErrorType -> Text
+etyMessage (ErrorType _ _ msg) = msg
+
+-- etyValidation
 
 isAsyncException :: (Exception e) => e -> Bool
 isAsyncException e =
