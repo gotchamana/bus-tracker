eed82f4f74d27fbe97af41d317e74f5eed3b6561
shootingstar <jinhung.c@gmail.com>
2026-07-12 22:22:35 +0800

    fix: Throw Exception

--- lib/Bus/Web/User/Api.hs
+++ lib/Bus/Web/User/Api.hs
@@ -2,20 +2,13 @@ module Bus.Web.User.Api (UserApi, userApi) where
 
 import Bus.App (AppM)
 import Bus.Database (BusTrackerDb (btUser), MonadDatabase (runBeam, withTransactionMode), busTrackerDb)
-import Bus.Exception (ApiException (..), etyInvalidRequestParameters)
-import Bus.Logging (logDebug, logDebug')
-import Control.Monad.Catch (MonadThrow (throwM))
 import Data.Aeson (Object)
 import Data.UUID (UUID)
-import Data.Void (Void)
 import Database.Beam (MonadIO (liftIO), all_, runSelectReturningList, select)
 import Database.PostgreSQL.Simple.Transaction (defaultTransactionMode)
-import Network.HTTP.Types.Status (status400)
 import Servant
 
 import Bus.Web.User.Service qualified as UserSvc
-import Data.List.NonEmpty qualified as NonEmpty
-import Data.Text qualified as Text
 
 type UserApi = "users" :> (ReqBody '[JSON] Object :> Post '[JSON] UUID :<|> Get '[JSON] Int)
 
@@ -24,19 +17,7 @@ userApi = registerUser :<|> getUser
 
 registerUser :: Object -> AppM UUID
 registerUser user = do
-    case UserSvc.validateNewUser user of
-        Left err -> logDebug' (NonEmpty.toList $ Text.pack . show <$> err)
-        Right u -> logDebug (Text.pack (show u))
-
-    -- UserSvc.save user
-
-    throwM
-        ApiException
-            { apiHttpStatus = status400
-            , apiErrorType = etyInvalidRequestParameters
-            , apiErrorDescription = Nothing
-            , apiErrorDetails = Nothing :: Maybe Void
-            }
+    UserSvc.validateNewUser user >>= UserSvc.save
 
 getUser :: AppM Int
 getUser = do
--- lib/Bus/Web/User/Service.hs
+++ lib/Bus/Web/User/Service.hs
@@ -5,22 +5,25 @@ module Bus.Web.User.Service (NewUser (..), save, validateNewUser) where
 
 import Bus.Database (MonadDatabase)
 import Bus.Database.Table.User (UserT (..))
+import Bus.Exception (ApiException (..), etyInvalidRequestParameters)
 import Bus.Validation.Aeson (parseObject)
-import Bus.Validation.Error (ValidationError)
 import Bus.Validation.Rerefined (NotEmpty, Trimmed, refineField)
+import Control.Monad.Catch (MonadThrow (throwM))
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Aeson (Object)
 import Data.Aeson.BetterErrors (asText, key, throwCustomError)
-import Data.List.NonEmpty (NonEmpty)
 import Data.Text (Text)
 import Data.Time (ZonedTime (zonedTimeToLocalTime), getZonedTime)
 import Data.UUID (UUID)
 import Data.UUID.V4 (nextRandom)
+import GHC.Stack (HasCallStack)
+import Network.HTTP.Types.Status (status400)
 import Rerefined (Refined, unrefine)
 import Rerefined.Predicates (And)
 import Valida (Validation (..))
 
 import Bus.Database.Repository.User qualified as UserRepo
+import Data.HashMap.Strict qualified as Map
 
 data NewUser = NewUser
     { usrAccount :: Refined (And Trimmed NotEmpty) Text
@@ -28,20 +31,30 @@ data NewUser = NewUser
     }
     deriving (Show)
 
-validateNewUser :: Object -> Either (NonEmpty ValidationError) NewUser
-validateNewUser = parseObject parser
+validateNewUser :: (HasCallStack, MonadThrow m) => Object -> m NewUser
+validateNewUser object = do
+    case parseObject parser object of
+        Left err ->
+            throwM
+                ApiException
+                    { apiHttpStatus = status400
+                    , apiErrorType = etyInvalidRequestParameters
+                    , apiErrorDescription = Nothing
+                    , apiErrorDetails = Just (Map.singleton @Text "violations" err)
+                    }
+        Right user -> pure user
   where
     parser = do
         account <- key "account" asText
         password <- key "password" asText
 
-        let f =
+        let result =
                 NewUser
                     <$> refineField "account" account
                     <*> refineField "password" password
 
-        case f of
-            Success n -> pure n
+        case result of
+            Success user -> pure user
             Failure err -> throwCustomError err
 
 save :: (MonadDatabase m) => NewUser -> m UUID
