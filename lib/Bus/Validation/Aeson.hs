fc74bc8677afc5eb28740e2f798888c74319b954
shootingstar <jinhung.c@gmail.com>
2026-07-12 20:35:12 +0800

    fix: Display path piece

--- lib/Bus/Validation/Aeson.hs
+++ lib/Bus/Validation/Aeson.hs
@@ -12,14 +12,16 @@ import Bus.Util.MessageCode (
  )
 import Bus.Validation.Error (ValidationError (..))
 import Data.Aeson (Object, Value (Object))
-import Data.Aeson.BetterErrors (ErrorSpecifics (..), JSONType (..), Parse, ParseError (..), parseValue)
+import Data.Aeson.BetterErrors (ErrorSpecifics (..), JSONType (..), Parse, ParseError (..), PathPiece (..), parseValue)
 import Data.Aeson.Text (encodeToLazyText)
 import Data.Bifunctor (Bifunctor (first))
 import Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.Text (Text)
 import Data.Text.Lazy (toStrict)
 import TextShow (TextShow (showt))
 
 import Data.HashMap.Strict qualified as Map
+import Data.List.NonEmpty qualified as NonEmpty
 import Data.Text qualified as Text
 
 parseObject :: Parse (NonEmpty ValidationError) a -> Object -> Either (NonEmpty ValidationError) a
@@ -27,55 +29,75 @@ parseObject parser object = first toValidationErrors (parseValue parser (Object 
 
 toValidationErrors :: ParseError (NonEmpty ValidationError) -> NonEmpty ValidationError
 toValidationErrors = \case
-    InvalidJSON err -> defaultValidationError (Text.pack err) errorValidationValidJsonSyntax [] :| []
-    BadSchema _ err -> case err of
-        KeyMissing key ->
-            defaultValidationError
-                ("Missing key \"" <> key <> "\"")
-                errorValidationRequiredJsonKey
-                [("key", key)]
-                :| []
-        OutOfBounds index ->
-            let index' = showt index
-             in defaultValidationError
-                    ("Array index out of bound: " <> index')
-                    errorValidationRequiredJsonArrayItem
-                    [("index", index')]
-                    :| []
-        WrongType jsonType value ->
-            let jsonType' = displayJsonType jsonType
-                value' = toStrict (encodeToLazyText value)
-             in defaultValidationError
-                    ("Expected type " <> jsonType' <> ", but got " <> value')
-                    errorValidationValidJsonType
-                    [("jsonType", jsonType'), ("value", value')]
-                    :| []
-        ExpectedIntegral num ->
-            let num' = showt num
-             in defaultValidationError
-                    ("Expected integer, but got " <> num')
-                    errorValidationValidJsonInteger
-                    [("nuumber", num')]
-                    :| []
-        FromAeson msg ->
-            defaultValidationError
-                (Text.pack msg)
-                errorValidationUnknownJsonError
-                []
-                :| []
-        CustomError valErrs -> valErrs
+    InvalidJSON err -> validationError Nothing (Text.pack err) errorValidationValidJsonSyntax [] :| []
+    BadSchema paths err ->
+        let field = displayPath paths
+         in case err of
+                KeyMissing key ->
+                    validationError
+                        field
+                        ("Missing key \"" <> key <> "\"")
+                        errorValidationRequiredJsonKey
+                        [("key", key)]
+                        :| []
+                OutOfBounds index ->
+                    let index' = showt index
+                     in validationError
+                            field
+                            ("Array index out of bound: " <> index')
+                            errorValidationRequiredJsonArrayItem
+                            [("index", index')]
+                            :| []
+                WrongType jsonType value ->
+                    let jsonType' = displayJsonType jsonType
+                        value' = toStrict (encodeToLazyText value)
+                     in validationError
+                            field
+                            ("Expected type " <> jsonType' <> ", but got " <> value')
+                            errorValidationValidJsonType
+                            [("jsonType", jsonType'), ("value", value')]
+                            :| []
+                ExpectedIntegral num ->
+                    let num' = showt num
+                     in validationError
+                            field
+                            ("Expected integer, but got " <> num')
+                            errorValidationValidJsonInteger
+                            [("nuumber", num')]
+                            :| []
+                FromAeson msg ->
+                    validationError
+                        field
+                        (Text.pack msg)
+                        errorValidationUnknownJsonError
+                        []
+                        :| []
+                CustomError valErrs -> valErrs
   where
-    defaultValidationError msg code args =
+    validationError field msg code args =
         ValidationError
-            { valField = Nothing
+            { valField = field
             , valMessage = msg
             , valMessageCode = code
             , valMessageArgs = Map.fromList args
             }
-    displayJsonType = \case
-        TyObject -> "object"
-        TyArray -> "array"
-        TyString -> "string"
-        TyNumber -> "number"
-        TyBool -> "boolean"
-        TyNull -> "null"
+
+displayPath :: [PathPiece] -> Maybe Text
+displayPath = \case
+    [] -> Nothing
+    (path : paths) -> case (path :| paths) >>= toText of
+        "." :| ps -> Just (Text.concat ps)
+        ps -> Just (Text.concat (NonEmpty.toList ps))
+  where
+    toText = \case
+        ObjectKey key -> "." :| [key]
+        ArrayIndex index -> "[" :| [showt index, "]"]
+
+displayJsonType :: JSONType -> Text
+displayJsonType = \case
+    TyObject -> "object"
+    TyArray -> "array"
+    TyString -> "string"
+    TyNumber -> "number"
+    TyBool -> "boolean"
+    TyNull -> "null"
